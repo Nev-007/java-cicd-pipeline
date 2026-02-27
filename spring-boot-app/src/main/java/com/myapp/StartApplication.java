1496ba04c1ffdde138e3538276f732e18e9b8e93
Nev-007 <nevulinux07@gmail.com>
2026-02-27 12:56:48 +0530

    Initial commit: Add spring-boot-cicd-project files

---
+++ spring-boot-app/src/main/java/com/myapp/StartApplication.java
@@ -0,0 +1,23 @@
+package com.myapp;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+@Controller
+public class StartApplication {
+
+    @GetMapping("/")
+    public String index(final Model model) {
+        model.addAttribute("title", "Spring Boot Application");
+        model.addAttribute("msg", "This application is deployed on Kubernetes using Argo CD");
+        return "index";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(StartApplication.class, args);
+    }
+
+}
